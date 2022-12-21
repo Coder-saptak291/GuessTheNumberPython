2eb1aaa00c0da324e28d7602ccf367092c1f0cb7
Saptak Majumdar <106856957+Coder-saptak291@users.noreply.github.com>
2022-12-21 20:32:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+n = random.randint(1,100)
+print(n)
+g = int(input("Enter your Guess: "))
+nog = 1
+
+while(g!=n):
+    if(g>n):
+        print("Enter a bit smaller number.")
+    elif(n>g):
+        print("Enter a bit greater number.")
+    g = int(input("Enter your Guess: "))
+    nog+=1
+
+print(f"You guessed it in {nog} attempts.")
